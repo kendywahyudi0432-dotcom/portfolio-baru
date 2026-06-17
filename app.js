b1fb9cb99693357343cb0fc02832c713d4a8d6ff
kendywahyudi0432-dotcom <kendywahyudi0432@gmail.com>
2026-06-17 19:38:07 +0700

    Upload proyek portofolio

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Kendi Wahyudi - Content Creator & Student Portfolio'
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
